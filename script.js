92882cf94ee1fe4b5db18e1c12643658905ceb4e
Luna Will <luwillhuber@gmail.com>
2025-10-06 21:51:39 -0300

    Agregando js en promo

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const promo = document.getElementById("promo1");
+
+    promo.onclick = () =>{
+        alert("Alimentate de manera saludable!");
+    }
+})
