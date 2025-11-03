1a96c81d1defabc08478b2404a93621bad21026b
Luna Will <luwillhuber@gmail.com>
2025-11-03 18:44:57 -0300

    Modificando imagenes verdura

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
-console.log("Hola mundo desde un archivo externo");
+document.addEventListener("DOMContentLoaded", () =>{
+    const promo = document.getElementById("promo1");
+
+    promo.onclick = () =>{
+        alert("Alimentate de manera saludable!");
+    }
+})
--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const promo = document.getElementById("promo1");
-
-    promo.onclick = () =>{
-        alert("Alimentate de manera saludable!");
-    }
-})
